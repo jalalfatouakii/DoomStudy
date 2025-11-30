2ced7c7e49251c306f5014b9883e68cff0f02c8b
Jalal Fatouaki <fatouaki.jalal@gmail.com>
2025-11-30 00:06:19 -0500

    basic settings

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <StatusBar style="light" />
+  </Stack>
 }
---
+++ constants/colors.ts
@@ -0,0 +1,13 @@
+
+export const Colors = {
+    text: '#ECEDEE',
+    background: '#151718',
+    tint: "#80f65cff",
+    icon: '#010203ff',
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: "#80f65c82",
+    backgroundSecondary: '#1A1C1E',
+    backgroundLighter: '#1E2022',
+
+};
+
