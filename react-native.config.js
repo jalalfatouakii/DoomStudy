ffa2d3e17d407734467687db331733f75e4a2ced
Jalal Fatouaki <fatouaki.jalal@gmail.com>
2025-12-06 14:35:08 -0500

    meh

---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dependencies: {
+        'react-native-widget-center': {
+            platforms: {
+                android: null, // Disable autolinking on Android
+            },
+        },
+    },
+};
